e5ff99f5de3cdd412ac5146a477338bc15206ecd
adearriba <adearriba@outlook.com>
2024-08-31 22:04:16 +0200

    Fix typing errors

--- localtunnel.d.ts
+++ localtunnel.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="node" />
-
 import { EventEmitter } from 'events';
 
-// Define the Options type used in various constructors
+// Define the options and other types used in the class
 interface TunnelOptions {
     host?: string;
     subdomain?: string;
@@ -18,23 +17,47 @@ interface TunnelOptions {
     local_port?: number;
 }
 
-interface TunnelCallback {
-    (error: Error | null, client?: Tunnel): void;
+interface TunnelInfo {
+    name: string;
+    url: string;
+    cached_url?: string;
+    max_conn: number;
+    remote_host: string;
+    remote_ip: string;
+    remote_port: number;
+    local_port: number;
+    local_host: string;
+    local_https: boolean;
+    local_cert?: string;
+    local_key?: string;
+    local_ca?: string;
+    allow_invalid_cert: boolean;
 }
 
-declare class Tunnel extends EventEmitter {
+type TunnelCallback = (error: Error | null, client?: Tunnel) => void;
+
+class Tunnel extends EventEmitter {
+    private _url: string;
+    private opts: TunnelOptions;
+    private closed: boolean;
+    private tunnelCluster: TunnelCluster; // Use the correct type
+
     constructor(opts?: TunnelOptions);
-    open(callback: TunnelCallback): void;
+    private _getInfo(body: any): TunnelInfo;
+    private _init(cb: TunnelCallback): void;
+    private _establish(info: TunnelInfo): void;
+
+    get url(): string;
+    open(cb: TunnelCallback): void;
     close(): void;
-    private _init(callback: (err: Error | null, info?: any) => void): void;
-    private _establish(info: any): void;
 }
 
-declare function localtunnel(arg1: number | TunnelOptions, arg2?: TunnelCallback | TunnelOptions, arg3?: TunnelCallback): Tunnel | Promise<Tunnel>;
+function localtunnel(arg1: number | TunnelOptions, arg2?: TunnelCallback | TunnelOptions, arg3?: TunnelCallback): Tunnel | Promise<Tunnel>;
 
-declare class TunnelCluster extends EventEmitter {
+class TunnelCluster extends EventEmitter {
     constructor(opts: TunnelOptions);
     open(): void;
 }
 
-export { localtunnel, Tunnel, TunnelCluster };
+export default localtunnel;
+export { Tunnel, TunnelCluster, TunnelOptions, TunnelInfo, TunnelCallback };
